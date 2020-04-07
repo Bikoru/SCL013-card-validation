62acbcfef10b9a487ba6af7fee7066443e7de7ac
kthcsk <csk.kath@gmail.com>
2020-04-07 11:27:59 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-import validator from './validator.js';
+/*import validator from './validator.js';
 
 console.log(validator);
+ Lo que esta arriba es del index.js original de laboratoria */
+
+document.getElementById("btnAll").addEventListener("click", function () {
+  let cardNumber = document.getElementById("numberCard").value;
+  let result = document.getElementById("result");
+
+  /* Validar*/
+  let receptor = Array.from(cardNumber);
+  let pairs = [];
+  let odd = [];
+  let newPairs = [];
+  let isValid = "";
+
+  for (var i = 0; i <= numeros.length; i++) {
+    numero = numeros[i];
+    suma += numero;
+  }
+
+  /* Mascara */
+
+  let maskify = "";
+  for (let i = 0; i < cardNumber.length; i++) {
+    if (i <= cardNumber.length - 5) {
+      maskify = maskify + "#";
+    } else {
+      maskify = maskify + cardNumber[i];
+    }
+
+    result.innerText = "¡Tu tarjeta número " + maskify + " es válida !";
+  }
+});
