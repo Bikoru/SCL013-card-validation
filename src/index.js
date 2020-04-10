af337d174b1575d3b9bff9b0231309bc0757cf06
Bikoru <victoria.moreno@live.cl>
2020-04-10 16:45:26 -0400

    Union de Validator.js e Index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,68 @@
-/*import validator from './validator.js';
+import validator from './validator.js';
 
-console.log(validator);
- Lo que esta arriba es del index.js original de laboratoria */
+//console.log(validator);
+ //Lo que esta arriba es del index.js original de laboratoria 
 
 document.getElementById("btnAll").addEventListener("click", function () {
   let cardNumber = document.getElementById("numberCard").value;
   let result = document.getElementById("result");
 
-  /* Validar*/
-  let receptor = Array.from(cardNumber);
-  let pairs = [];
-  let odd = [];
-  let newPairs = [];
-  let isValid = "";
 
-  for (var i = 0; i <= numeros.length; i++) {
-    numero = numeros[i];
-    suma += numero;
-  }
+
+  /* Validar*/
+
+
+let newNumber = validator.isValid(cardNumber);
+if(newNumber === true){
+  console.log("es verdadero");
+}else{
+  console.log("es falso");
+}
+
 
   /* Mascara */
 
-  let maskify = "";
-  for (let i = 0; i < cardNumber.length; i++) {
-    if (i <= cardNumber.length - 5) {
-      maskify = maskify + "#";
-    } else {
-      maskify = maskify + cardNumber[i];
-    }
+  let elMascarasLocas = validator.maskify(cardNumber);
 
-    result.innerText = "¡Tu tarjeta número " + maskify + " es válida !";
+  if(elMascarasLocas == "4"){
+    result.innerText = "fallaste perro";
+  }else{
+    result.innerText = "¡Tu tarjeta número " + elMascarasLocas + " es válida !";
   }
+    
 
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*
+  // el ciclo lo usamos para recorrer las posiciones del n° y ordenarlo a la inversa
+  for (let i = cardNumber.length - 1; i >= 0; i--) {
+    // obtenemos el caracter a travez de i
+    let cardCharacter = cardNumber.charAt(i);
+    // lo convertimos a n° entero para aplicar operaciones matematicas
+		let numberParse = parseInt(cardCharacter);
+
+    
+    numberValid = !numberValid;
+	}
+
+	return (numberCheck % 10) == 0;
+*/
+
+
+
+
 });
--- src/validator.js
+++ src/validator.js
@@ -1,8 +1,48 @@
 const validator = {
   isValid: (creditCardNumber) => {
+    
     let resultCardNumber = [];
-    for (let i = creditCardNumber.length - 1; i >= 0; i--) {}
-  },
-};
+    let numberPair = [];
+    let numberInpair = [];
 
-export default validator;
+    let result = false;
+
+
+    // guardamos los números de manera invertida 
+    for (let index = creditCardNumber.length - 1; index >=0; index--) {
+      // Obtenemos el caracter de cada número mediante el for y los almacenamos en la variable "cNumber"
+      let cNumber = creditCardNumber.charAt(index);
+      // Luego convertimos a número los caracteres y los guardamos en "numParse" 
+      let numParse = parseInt(cNumber);
+
+      console.log(typeof(numParse));
+    }
+
+
+   
+
+    result = true;
+
+    return result;
+  },
+
+
+
+  maskify: (creditCardNumber) =>{
+    let maskify = "";
+    for (let i = 0; i < creditCardNumber.length; i++) {
+      if (i <= creditCardNumber.length - 5) {
+        maskify = maskify + "#";
+      } else {
+        maskify = maskify + creditCardNumber[i];
+      }
+  
+      //result.innerText = "¡Tu tarjeta número " + maskify + " es válida !";
+    }
+
+    return maskify;
+
+  } 
+}
+
+export default validator
